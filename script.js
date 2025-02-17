8e9bad6fac0f81e688966ea7543b15d9ac9792a2
Jeico Arias <Ariasbritojeico7@gmail.com>
2025-02-17 19:50:25 -0400

    Proyecto terminado :v

---
+++ script.js
@@ -0,0 +1,54 @@
+const charactersEl = document.getElementById("characters");
+const nameFilterEl = document.getElementById("name-filters");
+const statusFilterEl = document.getElementById("status-filter");
+
+async function getCharacters(name, status) {
+	let url = "https://rickandmortyapi.com/api/character";
+
+	if (name || status) {
+		url += "?";
+		if (name) {
+			url += `name=${name}&`;
+		}
+
+		if (status) {
+			url += `status=${status}`;
+		}
+	}
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	return data.results;
+}
+
+async function displayCharacters(name, status) {
+	const characters = await getCharacters(name, status);
+
+	charactersEl.innerHTML = "";
+
+	for (let character of characters) {
+		const card = document.createElement("div");
+		card.classList.add("character-card");
+
+		card.innerHTML = `
+    <img src= "${character.image}"/>
+    <h2>${character.name}</h2>
+    <p> Status: ${character.status} </p>
+    <p> Especie: ${character.species} </p>
+    
+    `;
+
+		charactersEl.appendChild(card);
+	}
+}
+
+displayCharacters();
+
+nameFilterEl.addEventListener("input", () => {
+	displayCharacters(nameFilterEl.value, statusFilterEl.value);
+});
+
+statusFilterEl.addEventListener("change", () => {
+	displayCharacters(nameFilterEl.value, statusFilterEl.value);
+});
